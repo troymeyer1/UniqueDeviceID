8d5dce5e2b7a45b3e63db94c93a44e05edd467fa
Troy Meyer <troymeyer@att.net>
2020-08-25 16:25:46 -0500

    tweak for android 10 privacy changes

--- src/android/UniqueDeviceID.java
+++ src/android/UniqueDeviceID.java
@@ -55,32 +55,14 @@ public class UniqueDeviceID extends CordovaPlugin {
     protected void getDeviceId(){
         try {
             Context context = cordova.getActivity().getApplicationContext();
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-
-            String uuid;
             String androidID = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
-            String deviceID = tm.getDeviceId();
-            String simID = tm.getSimSerialNumber();
 
             if ("9774d56d682e549c".equals(androidID) || androidID == null) {
                 androidID = "";
             }
 
-            if (deviceID == null) {
-                deviceID = "";
-            }
-
-            if (simID == null) {
-                simID = "";
-            }
-
-            uuid = androidID + deviceID + simID;
-            uuid = String.format("%32s", uuid).replace(' ', '0');
-            uuid = uuid.substring(0, 32);
-            uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-
-            this.callbackContext.success(uuid);
-        }catch(Exception e ) {
+            this.callbackContext.success(androidID);
+        }catch (Exception e) {
             this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
         }
     }
